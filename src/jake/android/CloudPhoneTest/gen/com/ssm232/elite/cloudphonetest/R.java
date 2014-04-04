002f1e4ea8a8655e50a39a03acadcc10a0a17479
Jake Yoon <yjaeseok@gmail.com>
2014-04-04 16:09:08 +0900

    CloudPhone TestClient

--- src/jake/android/CloudPhoneTest/gen/com/ssm232/elite/cloudphonetest/R.java
+++ src/jake/android/CloudPhoneTest/gen/com/ssm232/elite/cloudphonetest/R.java
@@ -14,16 +14,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button_sample=0x7f060000;
-        public static final int button_test=0x7f060001;
-        public static final int layout=0x7f060002;
-        public static final int spinner_camera=0x7f060004;
-        public static final int spinner_size=0x7f060003;
+        public static final int btn_sample=0x7f060001;
+        public static final int btn_start=0x7f060000;
+        public static final int btn_stop=0x7f060002;
     }
     public static final class layout {
         public static final int layout_main=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int test=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
